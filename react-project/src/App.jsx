7ab3f6e197057cc778de399111cfa5c27325a3ac
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-01-10 00:10:45 -0300

    Trabalhando na mudança de tema com react

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,8 +1,38 @@
 import './App.css';
 import Main from './components/Main'
 import Footer from './components/Footer'
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+    // document.addEventListener('DOMContentLoaded', function() {  
+
+    //   console.log('aqui')
+
+    //   const switcherTheme = document.querySelector('[data-id="swith-theme"]')
+    //   const htmlTag = document.documentElement
+
+    //   const storageThemeValue = localStorage.getItem('theme')
+
+    //   if (storageThemeValue != null) {
+    //     htmlTag.setAttribute('data-theme', storageThemeValue)
+    //   }
+
+    //   if (htmlTag.getAttribute('data-theme') == 'dark') {
+    //     switcherTheme.checked = true
+    //   }
+
+    //   switcherTheme.addEventListener('click', () => toggleTheme() )
+
+    //   const toggleTheme = () => {      
+    //     const newTheme = switcherTheme.checked ? 'dark' : 'light'
+    //     htmlTag.setAttribute('data-theme', newTheme)
+
+    //     localStorage.setItem('theme', newTheme)
+    //   }
+
+    // })
+
   return (
     <div
       className="font-inter bg-white before:absolute before:inset-x-0 before:0 before:h-96 before:bg-very-pale-blue before:-z-10 text-very-dark-blue dark:bg-very-dark-blue dark:before:bg-very-dark-blue-top dark:text-white">
